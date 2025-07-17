435f3133bda65e9085e8957caf133f3afa9f4916
Mowriyan <mowriyan.c@gmail.com>
2025-07-17 21:36:58 +0530

    PROFILECARD

--- REACTPROJECT/reactweb/src/App.js
+++ REACTPROJECT/reactweb/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+
+      <div className="main">
+        <h1>MOWRIYAN C</h1>
+        <h2>Role</h2>
+        <p>I'm Final year Computer Science Student. I'm Pursuing at University College of Engineering, Ariyalur with a passion of Full Stack developer. I'm interested to learn MERN Stack to Become a developer. I know HTML, CSS, JS and interested to learn Full stack development.</p>
+        <h2>About</h2>
+        <p>Passionate and driven individual with a strong interest in technology and creative problem-solving.
+           Always eager to learn new skills and take on challenges with a positive mindset.
+           Committed to continuous growth, both personally and professionally.
+           Believe in teamwork, innovation, and making a meaningful impact.</p>
+        <button>More info</button>
+      </div>
+      <div className="header">
+        <img src="profile1.png.png" />
+      </div>
+    </header>
   );
 }
 
